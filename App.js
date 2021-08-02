74b352121837fc9ff0669833a846780db60af786
mukul-kc <mukul@karmacircles.com>
2021-08-02 18:23:26 +0530

    Set up redux

--- App.js
+++ App.js
@@ -6,15 +6,20 @@ import {
 
 import { enableScreens } from 'react-native-screens';
 // special package that enables native screens and improves performance
+import { createStore, combineReducers } from 'redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
+
 
 enableScreens()
+const rootReducer = combineReducers({ meals: mealsReducer });
+const store = createStore(rootReducer);
 
 const App = () => {
 
   return (
-    <MealsNavigator/>
+    <MealsNavigator />
   );
 };
 
